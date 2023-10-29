4523059d87afe9c00bfd72fe2e10fd2b93c7a8df
andreydmitr21 <andreydmitr20@gmail.com>
2023-10-29 13:09:52 +0100

    wip

--- back/back/celery.py
+++ back/back/celery.py
@@ -1,7 +1,11 @@
-# celery.py
+# celery -A back  worker -P threads --loglevel=info --beat
+
 from __future__ import absolute_import, unicode_literals
+
 import os
+
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back.settings")
@@ -19,3 +23,14 @@ celery_app.autodiscover_tasks()
 
 # We used CELERY_BEAT_SCHEDULER in settings.py instead of:
 # app.conf.beat_scheduler = ''django_celery_beat.schedulers.DatabaseScheduler'
+
+# configured so we can adjust scheduling
+# in the Django admin to manage all
+# Periodic Tasks like below
+# celery_app.conf.beat_schedule = {
+#     "multiply-task-crontab": {
+#         "task": "multiply_two_numbers",
+#         "schedule": crontab(hour=7, minute=30, day_of_week=1),
+#         "args": (16, 16),
+#     },
+# }
