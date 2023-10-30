bdf3cf17c7f0fc9f87f26de04813c5aae3295943
andreydmitr21 <andreydmitr20@gmail.com>
2023-10-30 22:16:43 +0100

    webflow js

---
+++ back/tasks/webflow/script.js
@@ -0,0 +1,151 @@
+PREFIX_NEWS1 = "news1_";
+const emailChange = (event, email) => {
+  sessionStorage.setItem(PREFIX_NEWS1 + "email", email.value);
+};
+const collectEmail = () => {
+  sessionStorage.setItem(PREFIX_NEWS1 + "email", "");
+  sessionStorage.setItem(PREFIX_NEWS1 + "news_type", "");
+  sessionStorage.setItem(PREFIX_NEWS1 + "days_in_week", "");
+  sessionStorage.setItem(PREFIX_NEWS1 + "phone", "");
+
+  let email = document.querySelector('input[type="email"]');
+  if (email === null) {
+    console.error("Cannot found email field");
+  }
+  console.dir(email);
+  email.addEventListener("change", (event) => emailChange(event, email));
+};
+
+const checkbox_change = (event, checkboxElement, type_label) => {
+  //   console.dir(checkboxElement);
+  let news_type = sessionStorage.getItem(PREFIX_NEWS1 + "news_type");
+  if (news_type === null) {
+    news_type = "";
+  }
+  news_type = news_type.replace(type_label, "");
+
+  if (checkboxElement.checked) {
+    news_type += type_label;
+  }
+  sessionStorage.setItem(PREFIX_NEWS1 + "news_type", news_type);
+};
+
+const radio_change = (event) => {
+  sessionStorage.setItem(PREFIX_NEWS1 + "days_in_week", event.target.value);
+};
+
+const phone_change = (event) => {
+  sessionStorage.setItem(PREFIX_NEWS1 + "phone", event.target.value);
+};
+
+const get_my_headlines = (event) => {
+  let headers = {
+    "Content-Type": "application/json",
+  };
+  //   headers["Authorization"] =
+  //     "Bearer " + sessionStorage.getItem(TOKEN_ACCESS_ITEM_NAME);
+  let request = {
+    method: "post",
+
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: headers,
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  };
+  request["body"] = JSON.stringify({
+    email: sessionStorage.getItem(PREFIX_NEWS1 + "email"),
+    news_type: sessionStorage.getItem(PREFIX_NEWS1 + "news_type"),
+    days_in_week: sessionStorage.getItem(PREFIX_NEWS1 + "days_in_week"),
+    phone: sessionStorage.getItem(PREFIX_NEWS1 + "phone"),
+  });
+  fetch("http://185.235.130.227:8000/news/api/client/0/", request)
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error(`Status code ${response.status} received`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data = data[0];
+      if (data["error"] !== "") {
+        throw new Error(data["error"]);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const collectInterests = () => {
+  console.log("collectInterests");
+
+  let itemw = document.getElementById("World-News");
+  itemw.addEventListener("change", (event) =>
+    checkbox_change(event, itemw, "w")
+  );
+  let itemt = document.getElementById("Tech-Innovation");
+  itemt.addEventListener("change", (event) =>
+    checkbox_change(event, itemt, "t")
+  );
+  let itemb = document.getElementById("Business-Finance");
+  itemb.addEventListener("change", (event) =>
+    checkbox_change(event, itemb, "b")
+  );
+  let items = document.getElementById("Science-Discovery");
+  items.addEventListener("change", (event) =>
+    checkbox_change(event, items, "s")
+  );
+  let itemh = document.getElementById("Health-Wellness");
+  itemh.addEventListener("change", (event) =>
+    checkbox_change(event, itemh, "h")
+  );
+  itemp = document.getElementById("Sports");
+  itemp.addEventListener("change", (event) =>
+    checkbox_change(event, itemp, "p")
+  );
+  let itemo = document.getElementById("Politics-Government");
+  itemo.addEventListener("change", (event) =>
+    checkbox_change(event, itemo, "o")
+  );
+  let iteme = document.getElementById("Environment-Sustainability");
+  iteme.addEventListener("change", (event) =>
+    checkbox_change(event, iteme, "e")
+  );
+  let itemn = document.getElementById("Entertainment-Culture");
+  itemn.addEventListener("change", (event) =>
+    checkbox_change(event, itemn, "n")
+  );
+  itemf = document.getElementById("Food-Lifestyle");
+  itemf.addEventListener("change", (event) =>
+    checkbox_change(event, itemf, "f")
+  );
+  let itema = document.getElementById("Art-Fashion");
+  itema.addEventListener("change", (event) =>
+    checkbox_change(event, itema, "a")
+  );
+  let radio_list = document.querySelectorAll('input[type="radio"]');
+  radio_list.forEach((radio_item) => {
+    radio_item.addEventListener("change", (event) => radio_change(event));
+  });
+  let phone = document.getElementById("phone-number");
+  phone.addEventListener("change", (event) => phone_change(event));
+  let btn_get_my_headlines = document.getElementById("btn-get-my-headlines");
+  btn_get_my_headlines.addEventListener("click", (event) =>
+    get_my_headlines(event)
+  );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  pageURL = window.location.href.toLowerCase();
+  console.log(pageURL);
+  if (pageURL === "https://headliness-radical-site.webflow.io/") {
+    collectEmail();
+  } else if (
+    pageURL === "https://headliness-radical-site.webflow.io/step-1-interests"
+  ) {
+    collectInterests();
+  }
+});
