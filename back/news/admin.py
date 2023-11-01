17ccf1ef83e307dd1b10cb3b37e515d8dd6ef372
andreydmitr21 <andreydmitr20@gmail.com>
2023-11-01 16:35:54 +0100

    hotfix clients admin

--- back/news/admin.py
+++ back/news/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from datetime import datetime, timedelta
+from config import config
 from .models import Clients
 
 
@@ -12,14 +13,44 @@ class ClientsAdmin(admin.ModelAdmin):
         "news_type",
         "days_in_week",
         "phone",
-        "utc_created",
-        "utc_updated",
-        "utc_payed",
+        "date_created",
+        "date_updated",
+        "date_payed",
     ]
     # list_filter = [
     #     "is_bad",
     # ]
     search_fields = ["email", "phone", "news_type"]
+
+    def date_created(self, obj):
+        if obj.utc_created:
+            utc_datetime = datetime.utcfromtimestamp(obj.utc_created) + timedelta(
+                hours=config.timezone
+            )
+            return utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        return "N/A"
+
+    date_created.short_description = f"Created UTC{config.timezone}"
+
+    def date_updated(self, obj):
+        if obj.utc_updated:
+            utc_datetime = datetime.utcfromtimestamp(obj.utc_updated) + timedelta(
+                hours=config.timezone
+            )
+            return utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        return "N/A"
+
+    date_updated.short_description = f"Updated UTC{config.timezone}"
+
+    def date_payed(self, obj):
+        if obj.utc_payed:
+            utc_datetime = datetime.utcfromtimestamp(obj.utc_payed) + timedelta(
+                hours=config.timezone
+            )
+            return utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        return "N/A"
+
+    date_payed.short_description = f"Payed UTC{config.timezone}"
 
 
 admin.site.register(Clients, ClientsAdmin)
